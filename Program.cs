756444ac6546863248047567546d63bab0d6efde
aasli <aasli@ASLINB>
2023-12-19 13:56:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace BTKAkademi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // struct - Usage
+            //Student std = new Student();
+            //std.Number = 10;
+            //std.Name = "Asli";
+            //std.Surname = "Atilgan";
+            //std.Gender = true;
+
+            // Alternative Usage
+            //var std2 = new Student()
+            //{
+                
+            //    Number = 20,
+            //    Name = "Kerem",
+            //    Surname = "Yildirim",
+            //    Gender = false,
+
+            //};
+
+            //// Alternative Usage 2
+
+
+
+            /// if we use on the Student.cs side we dont have to use these codes anymore
+            //Console.WriteLine($"{std.Number} " +
+            //    $"{std.Name} {std.Surname} " +
+            //    $"{std.Gender}");
+
+            //Console.WriteLine($"{std2.Number} " +
+            //   $"{std2.Name} {std2.Surname} " +
+            //   $"{std2.Gender}");
+
+            //Console.WriteLine($"{std3.Number} " +
+            //   $"{std3.Name} {std3.Surname} " +
+            //   $"{std3.Gender}");
+
+            //Console.WriteLine($"{std4.Number} " +
+            //   $"{std4.Name} {std4.Surname} " +
+            //   $"{std4.Gender}");
+
+            // We are able to make them out too if we use var std list
+            //Console.WriteLine(std);
+            //Console.WriteLine(std2);
+            //Console.WriteLine(std3);
+            //Console.WriteLine(std4);
+
+            var studentList = new List<Student>()
+            {
+                new Student(10, "Asli", "Atilgan", true),
+                new Student(20, "Kerem", "Yildirim", false),
+                new Student(40, "Hakan", "Karlıdağ"),
+            new Student(30, "Ekin", "Kara", true)
+        };
+
+            foreach (Student o in studentList) 
+                {
+                Console.WriteLine(o);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿namespace BTKAkademi
+{
+    // Describe
+    public struct Student
+    {
+        // Constructor method
+        public Student(int number, string name, string surname, bool gender = false)
+        {
+            //  when you write like this Alternative usage 2 worked
+            // Console.WriteLine("Automatically worked");
+            Number = number;
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+        }
+        // get - set used for reading and writing
+        //public int Number { get; set; } // Property
+        //public string Name { get; set; }
+        //public string Surname { get; set; }
+        //public bool Gender { get; set; }
+
+
+        public int Number { get; set; } // Property
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Gender { get; set; }
+
+        // Override -Geçersiz Kılmak / ezmek
+        public override string ToString()
+        {
+            return $"{Number,-5} " +
+                $"{Name,-10} " +
+                $"{Surname,-10} " +
+                string.Format("{0,-8}", Gender == false ? "Bay" : "Bayan");
+        }
+    }
+}
